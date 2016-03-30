2241c3beb28a723f15f8f6a9ffafa36a445902f6
vidhya <vidhu.chel@gmail.com>
2016-03-30 17:56:30 +0530

    argument->argv is added  with examples of more argument values in command line

--- test1.py
+++ test1.py
@@ -1,6 +1,7 @@
 import os
 import sys
-
+from os.path import exists
+'''
 print('As usual printing Hello World!')
 print(os.getcwd())
 print(sys.builtin_module_names)
@@ -12,3 +13,20 @@ ar1=sys.argv
 print("This is argument 1: "+ar1[1])
 print("This is argument 2: "+ar1[2])
 print("This is argument 3: "+ar1[3])
+
+print("Enter name:")
+print("Hi "+input('> ')+" welcome!")
+'''
+
+#create copy function as terminal command line
+copyinfo=sys.argv
+if copyinfo[1] == "copy":
+    if exists(copyinfo[2]):
+        with open(copyinfo[2]) as copyfile:
+            copyfile_content=copyfile.read()
+        tocopyfile=open(copyinfo[3],"w")
+        print(copyfile_content,file=tocopyfile)
+        tocopyfile.close()
+    else:
+        print("No file exists to copy!")
+
